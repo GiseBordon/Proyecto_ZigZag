b4a142a0f2c7905dc98ebef0b2379ce732c74a0d
Gisela Bordón <gisepbordon@gmail.com>
2022-06-25 19:41:48 -0300

    modificacion de pagina de contacto

---
+++ pages/envia.php
@@ -0,0 +1,29 @@
+﻿<?php
+$remitente = $_POST['email'];
+$destinatario = 'gisepbordon@gmail.com';
+$asunto = 'Consulta de ZigZag web page';
+if (!$_POST){
+?>
+
+<?php
+}else{
+	 
+    $cuerpo = "Nombre: " . $_POST["nombre"] . "\r\n"; 
+    $cuerpo = "Apellido: " . $_POST["apellido"] . "\r\n"; 
+    $cuerpo .= "Email: " . $_POST["email"] . "\r\n";
+	$cuerpo .= "Deja tu mensaje: " . $_POST["message"] . "\r\n";
+	//las líneas de arriba definen el contenido del mail. Las palabras que están dentro de $_POST[""] deben coincidir con el "name" de cada campo. 
+	// Si se agrega un campo al formulario, hay que agregarlo acá.
+
+    $headers  = "MIME-Version: 1.0\n";
+    $headers .= "Content-type: text/plain; charset=utf-8\n";
+    $headers .= "X-Priority: 3\n";
+    $headers .= "X-MSMail-Priority: Normal\n";
+    $headers .= "X-Mailer: php\n";
+    $headers .= "From: \"".$_POST['nombre']." ".$_POST['apellido']."\" <".$remitente.">\n";
+
+    mail($destinatario, $asunto, $cuerpo, $headers);
+    
+    include 'gracias.html'; //se debe crear un html que confirma el envío
+}
+?>
